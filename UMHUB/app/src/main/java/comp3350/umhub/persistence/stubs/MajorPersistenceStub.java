ed3d1f142b81c64d6fc6ce016f625f8ad25ace4e
Aseespal Singh Sehgal <47978666+lapse0310@users.noreply.github.com>
2021-02-23 12:05:55 -0600

    Update MajorPersistenceStub.java
    arranged majors in alphabetical order

--- UMHUB/app/src/main/java/comp3350/umhub/persistence/stubs/MajorPersistenceStub.java
+++ UMHUB/app/src/main/java/comp3350/umhub/persistence/stubs/MajorPersistenceStub.java
@@ -13,12 +13,12 @@ public class MajorPersistenceStub implements MajorPersistence {
     public MajorPersistenceStub(){
         this.majors = new ArrayList<>();
 
-        majors.add(new Major("Computer Science"));
         majors.add(new Major("Accounting"));
+        majors.add(new Major("Computer Science"));
         majors.add(new Major("Engineering"));
         majors.add(new Major("Mathematics"));
-        majors.add(new Major("Social Work"));
         majors.add(new Major("Philosophy"));
+        majors.add(new Major("Social Work"));
 
     }
 
