a2c690f9a017c8bbba04c860fa4a4ae9e3555e82
tranndt <tranndt@myumanitoba.ca>
2021-03-09 23:21:45 -0600

    Created CourseReview and relevant classes

---
+++ UMHUB/app/src/main/java/comp3350/umhub/business/AccessCourseReviews.java
@@ -0,0 +1,27 @@
+package comp3350.umhub.business;
+
+import java.util.Collections;
+import java.util.List;
+
+import comp3350.umhub.application.Services;
+import comp3350.umhub.objects.Course;
+import comp3350.umhub.objects.CourseReview;
+import comp3350.umhub.persistence.ICourseReviewPersistence;
+
+
+public class AccessCourseReviews implements IAccessCourseReviews{
+
+    private final ICourseReviewPersistence courseReviewPersistence;
+    private List<CourseReview> courseReviews;
+
+    public AccessCourseReviews(){
+        courseReviewPersistence = Services.getCourseReviewPersistence();
+        courseReviews = null;
+    }
+
+    @Override
+    public List<CourseReview> getCourseReviews(Course course) {
+        courseReviews = courseReviewPersistence.getCourseReviewsSequential();
+        return Collections.unmodifiableList(courseReviews);
+    }
+}
