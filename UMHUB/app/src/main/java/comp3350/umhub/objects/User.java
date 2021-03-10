f72545189f7fdf308a4173206f99c49421720b08
tranndt <tranndt@myumanitoba.ca>
2021-03-09 23:21:45 -0600

    Created CourseReview and relevant classes

---
+++ UMHUB/app/src/main/java/comp3350/umhub/objects/User.java
@@ -0,0 +1,9 @@
+package comp3350.umhub.objects;
+
+public class User {
+    private String uid;
+
+    public String getUid() {
+        return uid;
+    }
+}
---
+++ UMHUB/app/src/main/java/comp3350/umhub/persistence/stubs/CourseReviewPersistenceStub.java
@@ -0,0 +1,6 @@
+package comp3350.umhub.persistence.stubs;
+
+import comp3350.umhub.persistence.ICourseReviewPersistence;
+
+public class CourseReviewPersistenceStub implements ICourseReviewPersistence {
+}
